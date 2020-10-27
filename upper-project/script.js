b4bc4335dc6db69bc96a0a8c0d22a7e1fa646fbd
opedroaravena <pedroaravena.contato@gmail.com>
2020-10-27 10:56:56 -0300

    Copy button was added

---
+++ upper-project/script.js
@@ -0,0 +1,14 @@
+function convertUpper(){
+    let text = document.getElementById('textBox').value;
+    let result = text.toUpperCase();
+    document.getElementById('converterDiv').innerHTML = result;
+}
+
+function copyToClipboard() {
+  var copyText = document.getElementById("converterDiv");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  alert("Copied!");
+}
+  
